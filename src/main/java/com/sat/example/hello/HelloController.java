993fc447091530593beb32e5ef844df0ed869f75
skumarind <satish.1986.kumar@gmail.com>
2018-03-15 00:19:39 +0000

    sample program rnnign

---
+++ src/main/java/com/sat/example/hello/HelloController.java
@@ -0,0 +1,13 @@
+package com.sat.example.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Satish on 3/15/2018.
+ */
+public class HelloController {
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+}
